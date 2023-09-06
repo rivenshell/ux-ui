932f7fcf71effa22f7d8821c556df0d7e3fbbc0d
Rivenshell Salomon <rivenshell111@gmail.com>
2023-09-06 07:42:54 -0400

    update

---
+++ app/reflection/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from "react"
+
+export default function reflection() {
+  return (
+    <>
+      <div className="text-center">reflection</div>
+    </>
+  )
+}
---
+++ app/samispace/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from "react"
+import Sidebarm from "../components/Sidebarm"
+
+export default function samispace() {
+  return (
+    <>
+      <div className="text-center mx-4">
+        <Sidebarm />
+        Samispace
+      </div>
+    </>
+  )
+}
