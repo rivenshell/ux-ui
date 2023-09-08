13ae1ebe0062d487e10b23e0867e8c63bad5a99c
Rivenshell <admin@Darth.local>
2023-09-08 11:21:23 -0400

    nav update

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,11 @@
 "use client"
 import React from "react"
+import Nav from "../components/Nav"
 
 export default function notes() {
-  return <></>
+  return (
+    <>
+      <Nav />
+    </>
+  )
 }
